abb71c97a0596d6eb40423a56cb9e6c5efe21fbe
anatoly popov <popov.anatoly@gmail.com>
2019-05-22 18:42:51 +0300

    deploy1

--- fabfile.py
+++ fabfile.py
@@ -12,3 +12,20 @@ def backup():
     local('git commit -m "{}"'.format(comment))
     print(green('pushing master...'))
     local('git push -u origin master')
+
+def deploy():
+    local("python manage.py test")
+    local('pip freeze > requirements.txt')
+    local('git pull')
+    local('git add .')
+    print(green("enter your comment:"))
+    comment = input()
+    local('git commit -m "{}"'.format(comment))
+    local('git push -u origin master')
+    #switch_debug("True", "False")
+    local('python manage.py collectstatic --noinput')
+    #switch_debug("False", "True")
+
+def migrate():
+    local('python manage.py makemigrations')
+    local('python manage.py migrate')
